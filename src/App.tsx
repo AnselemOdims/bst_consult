0ab3bfbd241f61231aac68dfd551e9a84a67b028
Anselem Odimegwu <odimegwu@systemspecs.com.ng>
2022-05-17 19:19:46 +0100

    Feat: complete the headline section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import TopHeader from './components/header/TopHeader';
 import BottomHeader from './components/header/BottomHeader';
+import Headline from './components/Headline';
 
 function App() {
   return (
     <div className="App">
       <TopHeader />
       <BottomHeader />
-      <h1>Headline Section</h1>
+      <Headline />
     </div>
   );
 }
---
+++ src/components/Headline.tsx
@@ -0,0 +1,17 @@
+
+const Healine = () => {
+    return (
+        <section className="headline">
+            <div>
+                <h1>We make real estate easier</h1>
+                <p>From consulting to management and development, we cover all the bases
+so you don't have to.</p>
+                <div>
+                    <a>MORE ABOUT US</a>
+                </div>
+            </div>
+        </section>
+      );
+}
+ 
+export default Healine;
